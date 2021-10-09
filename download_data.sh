eea3713fbc7655e669c9bb849ad6a40bf03e3193
megaelius <abad.elias00@gmail.com>
2021-10-09 14:54:51 +0200

    download data

--- download_data.sh
+++ download_data.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# Download HDF5 for indoor 3d semantic segmentation (around 1.6GB)
-wget https://shapenet.cs.stanford.edu/media/indoor3d_sem_seg_hdf5_data.zip --no-check-certificate
-unzip indoor3d_sem_seg_hdf5_data.zip
-rm indoor3d_sem_seg_hdf5_data.zip
+# Download/unzip labels
+d='./Data' # unzip directory
+url=https://shapenet.cs.stanford.edu/media/
+f='indoor3d_sem_seg_hdf5_data.zip'
+echo 'Downloading' $url$f ' ...'
+curl -L --insecure $url$f -o $f && unzip -q $f -d $d && rm $f &
